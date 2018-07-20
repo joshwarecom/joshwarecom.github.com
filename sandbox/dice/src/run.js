c06c97c672a82925dd0db562e0619147c21ac396
joshwarecom <josh@joshware.com>
2018-07-20 08:09:46 -0400

    Update run.js

--- sandbox/dice/src/run.js
+++ sandbox/dice/src/run.js
@@ -7,7 +7,7 @@ else {
 RunPrefixMethod(e, "RequestFullScreen");
 }
 e = document.getElementById('gif');
-e.src = "http://joshware.com/sandbox/dice/assets/skull.gif";
+e.src = "https://joshware.com/sandbox/dice/assets/skull.gif";
 e.style.width = "100%";
 e.style.height="100%";
 toggleDisplay("dice",0);
