b10fb3c10623aa307f02222a14f6f33ad52cfc2b
joshwarecom <josh@joshware.com>
2018-07-20 08:07:18 -0400

    Update run.js

--- sandbox/dice/src/run.js
+++ sandbox/dice/src/run.js
@@ -1,5 +1,16 @@
-function xyz() {
-alert(5);
+var autoRescale = true;
+
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+    autoRescale = false;
+}
+
+Game = {
+  start: function() {
+    // Start crafty and set a background color so that we can see it's working
+    Crafty.init();
+    //Crafty.background('rgb(0, 128, 0)');
+    Crafty.scene('Loading');   
+  }
 }
 
 var e = document.getElementById("fullscreen");
