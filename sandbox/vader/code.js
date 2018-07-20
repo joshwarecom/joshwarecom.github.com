120a891cc767356be3130f083e8880b85130208a
joshwarecom <josh@joshware.com>
2018-07-20 10:38:17 -0400

    Update code.js

--- sandbox/vader/code.js
+++ sandbox/vader/code.js
@@ -13008,3 +13008,39 @@ Crafty.extend({
 
 },{"./core.js":10}]},{},[11]);
 
+var preloadSound = true;
+var preloadGraphics = true;
+
+var audioList = {
+        "laugh": ["https://joshware.com/sandbox/dice/assets/evilLaugh.aac", "https://joshware.com/sandbox/dice/assets/evilLaugh.ogg", "https://joshware.com/sandbox/dice/assets/evilLaugh.mp3"],
+};
+
+var graphicsList = ["https://joshware.com/sandbox/dice/assets/skull.gif"];
+
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i)) || (navigator.userAgent.match(/iOS/i))) {
+    preloadSound = false;
+}
+
+var assetsObj = {
+    "audio": preloadSound ? audioList : null,
+    "images": preloadGraphics ? graphicsList : null
+};
+
+Crafty.scene('Loading', function() {
+    Crafty.load(assetsObj,
+        function() {
+            //when loaded
+            if (!preloadSound) {
+                 Crafty.audio.add(audioList);
+            }
+        },
+
+        function(e) {
+            //progress
+        },
+
+        function(e) {
+            //error
+        }
+    );
+});
