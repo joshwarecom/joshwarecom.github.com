e1646b9f0640f3a4e352080c2401f8c6e379fcbc
joshwarecom <josh@joshware.com>
2021-04-21 17:18:31 -0400

    Delete sandbox/if/parchment-ifcomp directory

--- sandbox/if/parchment-ifcomp/src/common/ie.js
+++
@@ -1,9 +0,0 @@
-// Show an error in Internet Explorer
-
-function show_error()
-{
-    $('#errorcontent').text('Sorry, but Parchment does not support Intenet Explorer. Please use a more modern browser.')
-    $('#errorpane').show()
-}
-
-$(show_error)
